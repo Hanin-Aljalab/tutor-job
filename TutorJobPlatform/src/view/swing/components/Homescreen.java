ab64947a12960d72887de34a26e950d33065d4b1
3008816 <3008816@stud.hs-mannheim.de>
2024-05-07 20:17:15 +0200

    changed Button

--- TutorJobPlatform/src/view/swing/components/Homescreen.java
+++ TutorJobPlatform/src/view/swing/components/Homescreen.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.prefs.Preferences;
 
 public class Homescreen extends JFrame {
     private User user;
@@ -29,18 +28,19 @@ public class Homescreen extends JFrame {
         constraints.insets = new Insets(10, 10, 10, 10);
 
         JPanel top = new JPanel(); //----------------------------------------------------
-        top.setBounds(0,0,700,50);
+        top.setBounds(0, 0, 700, 50);
         top.setLayout(new GridBagLayout());
         add(top);
         top.setBackground(Color.GRAY);
 
         name = new JLabel("Name: " + user.getName() + " " + user.getSurname());
+
         constraints.gridx = 0;
         constraints.gridy = 0;
         constraints.weightx = 1;
         top.add(name, constraints);
 
-        if(user.getClass() == Student.class) {
+        if (user.getClass() == Student.class) {
             matrikelnumber = new JLabel("mtr: " + ((Student) user).getMatNummer());
             constraints.gridx = 1;
             constraints.gridy = 0;
@@ -48,12 +48,12 @@ public class Homescreen extends JFrame {
         }
 
         JSeparator separator = new JSeparator();
-        separator.setBounds(0,50,700,4);
+        separator.setBounds(0, 50, 700, 4);
         separator.setBackground(Color.BLUE);
         add(separator);
 
         JPanel bottom = new JPanel(); // ----------------------------------------------------
-        bottom.setBounds(0,58,700,300);
+        bottom.setBounds(0, 58, 700, 300);
         bottom.setLayout(new GridLayout());
         add(bottom, BorderLayout.CENTER);
 
@@ -62,42 +62,45 @@ public class Homescreen extends JFrame {
         bottomLeft.setBackground(Color.lightGray);
         bottom.add(bottomLeft);
 
-        if(((Student) user).isChoiceMade()) {
+        if (!((Student) user).isChoiceMade()) {
             chooseYourPreference = new JButton("Choose your Preference");
-            constraints.gridx = 0;
-            constraints.gridy = 0;
 
             chooseYourPreference.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     new PreferencePopup((Student) user);
+                    ((Student) user).setChoiceMade(true);
+                    chooseYourPreference.setEnabled(false);
                 }
             });
-
             bottomLeft.add(chooseYourPreference, constraints);
         }
-        // TODO neue Methode in Student die prüft ob schon preference gewählt wurde
+
+            // TODO neue Methode in Student die prüft ob schon preference gewählt wurde
 
 
-        JPanel bottomRight = new JPanel(); // ...........................
-        bottomRight.setLayout(new GridBagLayout());
-        //bottomRight.setBackground(Color.white);
-        bottom.add(bottomRight);
+            JPanel bottomRight = new JPanel(); // ...........................
+            bottomRight.setLayout(new GridBagLayout());
+            //bottomRight.setBackground(Color.white);
+            bottom.add(bottomRight);
 
-        infoPreference = new JLabel("HIER STEHEN DIE PRÄFERENZEN"); // TODO: Listen von Student ausgeben mit SwingList
-        constraints.gridx = 2;
-        constraints.gridy = 0;
-        bottomRight.add(infoPreference, constraints);
+            infoPreference = new JLabel("HIER STEHEN DIE PRÄFERENZEN"); // TODO: Listen von Student ausgeben mit SwingList
 
-        setVisible(true);
+            constraints.gridx = 2;
+            constraints.gridy = 0;
+            bottomRight.add(infoPreference, constraints);
+
+            setVisible(true);
+        }
+
+        public static void main (String[]args){
+            Student s1 = new Student("Markus", "W", "1234", 3008816);
+            Teacher t1 = new Teacher("Markus", "W", "1234");
+            new Homescreen(t1);
+        }
+
+
+        //TODO: PrefPOPup schließt wenn x --> lieber nur das Fenster schließen
+
+
     }
-
-    public static void main(String[] args) {
-        Student s1 = new Student("Markus", "W", "1234", 3008816);
-        Teacher t1 = new Teacher("Markus", "W", "1234");
-        new Homescreen(s1);
-    }
-
-    //TODO: PrefPOPup schließt wenn x --> lieber nur das Fenster schließen
-}
-
 
