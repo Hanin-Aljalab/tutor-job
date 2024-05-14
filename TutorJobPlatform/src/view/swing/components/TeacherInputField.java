d469c4187b618134dc0e6590969b272cd8fc2965
Julia Aktories <3012052@stud.hs-mannheim.de>
2024-05-14 15:41:55 -0400

    add main to teacherinputfield for testing

--- TutorJobPlatform/src/view/swing/components/TeacherInputField.java
+++ TutorJobPlatform/src/view/swing/components/TeacherInputField.java
@@ -87,4 +87,9 @@ public class TeacherInputField extends JFrame {
 
         setVisible(true);
     }
+
+    public static void main(String[] args) {
+        Teacher stg = new Teacher("Jess", "Steinberger", "12345", "Prof. Dr.");
+        TeacherInputField field = new TeacherInputField(stg);
+    }
 }
