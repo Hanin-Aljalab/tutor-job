f7646a42d8a01e367f937e8e0c2017cae6e7767f
Markus Winklhofer <3008816@stud.hs-mannheim.de>
2024-05-23 18:58:04 +0000

    maby del RegPopUpTest

--- TutorJobPlatform/test/view/swing/components/RegistrationPopupTest.java
+++ TutorJobPlatform/test/view/swing/components/RegistrationPopupTest.java
@@ -11,68 +11,68 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationPopupTest {
-    private RegistrationPopup registrationPopup;
-    private AppData data;
-
-    @BeforeEach
-    void setUp() {
-        registrationPopup = new RegistrationPopup();
-        data = new AppData();
-
-        registrationPopup.setFirstNameField("Markus");
-        registrationPopup.setLastNameField("Winklhofer");
-        registrationPopup.setPasswordField("password");
-        registrationPopup.setPasswordConfirmField("password");
-        registrationPopup.setStudNumberField("1234567");
-        registrationPopup.setStudyPathDropdown("IMB");
-        registrationPopup.setRoleDropdown("Student*in");
-    }
-
-    @Test
-    void testUserAlreadyExistsException() {
-        Student existingStudent = new Student("Markus", "Winklhofer",
-                "password", "1234567", "IMB");
-        data.addUser(existingStudent);
-
-        assertThrows(UserAlreadyExistsException.class, () -> {
-            registrationPopup.transmitData();
-        });
-    }
-
-    @Test
-    void testStudentNumberInvalidException() {
-        registrationPopup.setStudNumberField("abc");
-
-        assertThrows(StudentNumberInvalidException.class, () -> {
-            registrationPopup.transmitData();
-        });
-    }
-
-    @Test
-    void testPasswordsNotIdenticalException() {
-        registrationPopup.setPasswordConfirmField("differentPassword");
-
-        assertThrows(PasswordsNotIdenticalException.class, () -> {
-            registrationPopup.transmitData();
-        });
-    }
-
-    @Test
-    void testInvalidInputException() {
-        registrationPopup.setFirstNameField("");
-
-        assertThrows(InvalidInputException.class, () -> {
-            registrationPopup.transmitData();
-        });
-    }
-
-    @Test
-    void testTeacherIdInvalidException() {
-        registrationPopup.setRoleDropdown("Dozent*in");
-        registrationPopup.setTeacherIdField("WKL1");
-
-        assertThrows(TeacherIdInvalidException.class, () -> {
-            registrationPopup.transmitData();
-        });
-    }
+//    private RegistrationPopup registrationPopup;
+//    private AppData data;
+//
+//    @BeforeEach
+//    void setUp() {
+//        registrationPopup = new RegistrationPopup();
+//        data = new AppData();
+//
+//        registrationPopup.setFirstNameField("Hans");
+//        registrationPopup.setLastNameField("Peter");
+//        registrationPopup.setPasswordField("password");
+//        registrationPopup.setStudNumberField("1234567");
+//        registrationPopup.setStudyPathDropdown("IMB");
+//        registrationPopup.setRoleDropdown("Student*in");
+//
+//    }
+//
+//    @Test
+//    void testUserAlreadyExistsException() {
+//        Student existingStudent = new Student("Hans", "Peter",
+//                "password", "1234567", "IMB");
+//        data.addUser(existingStudent);
+//
+//        assertThrows(UserAlreadyExistsException.class, () -> {
+//            registrationPopup.transmitData();
+//        });
+//    }
+//
+//    @Test
+//    void testStudentNumberInvalidException() {
+//        registrationPopup.setStudNumberField("abc");
+//
+//        assertThrows(StudentNumberInvalidException.class, () -> {
+//            registrationPopup.transmitData();
+//        });
+//    }
+//
+//    @Test
+//    void testPasswordsNotIdenticalException() {
+//        registrationPopup.setPasswordConfirmField("differentPassword");
+//
+//        assertThrows(PasswordsNotIdenticalException.class, () -> {
+//            registrationPopup.transmitData();
+//        });
+//    }
+//
+//    @Test
+//    void testInvalidInputException() {
+//        registrationPopup.setFirstNameField("");
+//
+//        assertThrows(InvalidInputException.class, () -> {
+//            registrationPopup.transmitData();
+//        });
+//    }
+//
+//    @Test
+//    void testTeacherIdInvalidException() {
+//        registrationPopup.setRoleDropdown("Dozent*in");
+//        registrationPopup.setTeacherIdField("WKL1");
+//
+//        assertThrows(TeacherIdInvalidException.class, () -> {
+//            registrationPopup.transmitData();
+//        });
+//    }
 }
