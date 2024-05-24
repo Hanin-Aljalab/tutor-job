d1a1b112d51b655eb740a8ede0bcb2f68e53a313
3008816 <3008816@stud.hs-mannheim.de>
2024-05-25 00:48:41 +0200

    not finished StudentHomescreenTest

--- TutorJobPlatform/test/view/swing/components/homescreens/StudentHomescreenTest.java
+++ TutorJobPlatform/test/view/swing/components/homescreens/StudentHomescreenTest.java
@@ -1,24 +1,56 @@
 package view.swing.components.homescreens;
 
+import controller.App;
+import model.AppData;
 import model.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StudentHomescreenTest {
 
-//    @Test
-//    void removePreferencesTest() {
-//        Student s1 = new Student("Markus", "Winklhofer", "12345", "3008816", "IMB");
-//        StudentHomescreen homescreen = new StudentHomescreen(s1);
-//
-//        s1.addPreference("MA1", "Kurse");
-//        s1.addPreference("Todorov", "Dozent*innen");
-//        s1.addPreference("PR2", "Kurse");
-//
-//        homescreen.removePreferences();
-//
-//        assertTrue(s1.getLecturePref().isEmpty(), "Expected lecture preferences to be empty");
-//        assertTrue(s1.getTeacherPref().isEmpty(), "Expected teacher preferences to be empty");
-//    }
+    private AppData appData;
+    private Student s1;
+    private StudentHomescreen homescreen;
+    private ArrayList<String> lecturePrefBefore;
+    private ArrayList<String> teacherPrefBefore;
+    private ArrayList<String> lecturePrefAfter;
+    private ArrayList<String> teacherPrefAfter;
+
+    @BeforeEach
+    void setUp() {
+        appData = new AppData();
+        System.out.println("AppData initialized: " + true);
+
+        s1 = new Student("Markus", "Winklhofer", "12345", "3008816", "IMB");
+        s1.addPreference("MA1", "Kurse");
+        s1.addPreference("Todorov", "Dozent*innen");
+        s1.addPreference("PR2", "Kurse");
+        appData.addUser(s1);
+        System.out.println("Student initialized: " + s1);
+
+        App.setData(appData);
+        System.out.println("AppData set in App class: " + App.getData());
+
+        homescreen = new StudentHomescreen(s1);
+        System.out.println("StudentHomescreen initialized with student: " + App.getData().getStudents());
+
+        lecturePrefBefore = new ArrayList<>(s1.getLecturePref());
+        teacherPrefBefore = new ArrayList<>(s1.getTeacherPref());
+
+    }
+
+    @Test
+    void removePreferences() {
+        homescreen.removePreferences();
+        lecturePrefAfter = new ArrayList<>(s1.getLecturePref());
+        teacherPrefAfter = new ArrayList<>(s1.getTeacherPref());
+
+        assertEquals(lecturePrefAfter, lecturePrefBefore);
+        assertEquals(teacherPrefAfter, teacherPrefBefore);
+
+    }
 }
